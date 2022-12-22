40f6d0b3da396a535100545d5b1d722080b802bf
BRYAN(Jingchen) TANG <tangtang1228@ucla.edu>
2022-12-22 03:32:42 -0800

    format files

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -3,27 +3,34 @@
   var slides = { highlight_style: "dracula", theme: "black" };
 
   // <stdin>
-  var enabledPlugins = [RevealMarkdown, RevealHighlight, RevealSearch, RevealNotes, RevealMath.MathJax3, RevealZoom];
-  var isObject = function(o) {
+  var enabledPlugins = [
+    RevealMarkdown,
+    RevealHighlight,
+    RevealSearch,
+    RevealNotes,
+    RevealMath.MathJax3,
+    RevealZoom,
+  ];
+  var isObject = function (o) {
     return o === Object(o) && !isArray(o) && typeof o !== "function";
   };
-  var isArray = function(a) {
+  var isArray = function (a) {
     return Array.isArray(a);
   };
-  var toCamelCase = function(s) {
-    return s.replace(/([-_][a-z])/gi, function(term) {
+  var toCamelCase = function (s) {
+    return s.replace(/([-_][a-z])/gi, function (term) {
       return term.toUpperCase().replace("-", "").replace("_", "");
     });
   };
-  var keysToCamelCase = function(o) {
+  var keysToCamelCase = function (o) {
     if (isObject(o)) {
       const n = {};
-      Object.keys(o).forEach(function(k) {
+      Object.keys(o).forEach(function (k) {
         n[toCamelCase(k)] = keysToCamelCase(o[k]);
       });
       return n;
     } else if (isArray(o)) {
-      return o.map(function(i) {
+      return o.map(function (i) {
         return keysToCamelCase(i);
       });
     }
@@ -48,21 +55,26 @@
       var background = mslide.slideBackgroundElement;
       var currentHorizontalIndex = Reveal.getState()["indexh"];
       var diagramSlideIndex = Reveal.getIndices(mslide)["h"];
-      if (!diag.hasAttribute("data-processed") && background.hasAttribute("data-loaded") && background.style.display === "block" && diagramSlideIndex - currentHorizontalIndex <= 1)
+      if (
+        !diag.hasAttribute("data-processed") &&
+        background.hasAttribute("data-loaded") &&
+        background.style.display === "block" &&
+        diagramSlideIndex - currentHorizontalIndex <= 1
+      )
         return mslide;
     }
     return null;
   }
   function renderMermaidSlides() {
     var diagramSlides = Reveal.getSlides().filter(mermaidSlidesReadyToRender);
-    diagramSlides.forEach(function(item) {
+    diagramSlides.forEach(function (item) {
       mermaid.init(item.querySelector(".mermaid"));
     });
   }
-  Reveal.on("slidechanged", function() {
+  Reveal.on("slidechanged", function () {
     renderMermaidSlides();
   });
-  Reveal.on("Ready", function() {
+  Reveal.on("Ready", function () {
     if (Reveal.isReady()) {
       renderMermaidSlides();
     }
